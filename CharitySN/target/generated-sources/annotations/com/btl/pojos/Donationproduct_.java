62ab4a8d63bc0841c7a52dc4462d961c50b62ff2
co hung <trancohung2016@gmail.com>
2024-06-04 13:46:40 +0700

    Update plugin

--- CharitySN/target/generated-sources/annotations/com/btl/pojos/Donationproduct_.java
+++ CharitySN/target/generated-sources/annotations/com/btl/pojos/Donationproduct_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-11-04T13:41:53")
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2024-06-04T13:43:41")
 @StaticMetamodel(Donationproduct.class)
 public class Donationproduct_ { 
 
