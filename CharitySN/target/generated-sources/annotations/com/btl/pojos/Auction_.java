d7a76967f7332b0bf4ca1defb6d5cbbfbb8ad965
co hung <trancohung2016@gmail.com>
2024-06-04 13:46:40 +0700

    cap nhat plugin

--- CharitySN/target/generated-sources/annotations/com/btl/pojos/Auction_.java
+++ CharitySN/target/generated-sources/annotations/com/btl/pojos/Auction_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-11-04T13:41:53")
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2024-06-04T13:43:41")
 @StaticMetamodel(Auction.class)
 public class Auction_ { 
 
